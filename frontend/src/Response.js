fbbaa0b3ec472ebac54a1d70dabf2f0eccaf2c58
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-03-22 12:02:28 -0600

    Updated responses to be able to have nested responses.

--- frontend/src/Response.js
+++ frontend/src/Response.js
@@ -1,9 +1,94 @@
-
+import {useState,useEffect} from "./react"
 
 function Response(props) {
 
+	const [responses,changeResponses] = useState([]);
+	
+	let id = props.id
+	let url = process.env.URL || "http://localhost:3002"
+	const URL = url + "/nestedReply/"+ props.id
+
+	let description = props.description;
+
+	//the response button
+	const [data,changeData] = useState("");
+
+	//console.log("my id is :"+id);
+
+	function handleDataUpdate(event) {
+		changeData(d => d = event.target.value);
+	}
+
+
+	function submitResponse() {
+	
+		let theData = data;
+		fetch(url + "/reply", {
+			method: "POST",
+			headers: {"Content-Type":"application/json"},
+			body:JSON.stringify( {
+				reply_id:id,
+				description:data,//possible variable shadow bug here
+				post_id: undefined,
+			}
+		)}).then(response => {
+			if (!response.ok) {	
+				throw new Error(`Response status: ${response.status}`)
+			}
+			else return response.json();
+
+		}).then(data => 
+			changeResponses( prev => 
+			prev = [...prev,{description:data,id:data.replyId}] //adds the new document to the array 
+			) 
+		).catch(err => console.log(err));
+
+
+
+	}
+
+
+
+	function getResponses() {
+	
+		fetch(URL).then(response => {
+		
+			if (!response.ok) {	
+				throw new Error(`Response status: ${response.status}`)
+			}
+			else return response.json();
+
+		}).then(data => { 
+			
+			if (data.reply.length == 0)  {return;}
+
+			changeResponses( prev => 
+			prev = prev.concat(data.reply) ) 
+		}).catch(err => console.log(err));
+
+	}
+
+
+
+
+
+
+
 	return (
-		<p> {props.data} {props.timestamp} </p>
+		<>
+		<p> {props.description}  </p>
+		<ul> 
+		{responses.map( response => 
+			<Response 
+			id = {response.id}
+			description = {response.description} 
+			timestamp =  {response.timestamp}
+			key = {response.id}/>)}
+		</ul>
+		<p>enter in a response</p>
+		<input type = "text" onChange = {handleDataUpdate}/>
+		<button onClick = {submitResponse}> submit </button>
+		</>
 	);
 
 }
