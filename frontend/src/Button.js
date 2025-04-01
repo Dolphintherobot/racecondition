8a003f3573afc152ab118b1f01e141f3fb7daf14
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-01 13:24:09 -0600

    Fixed button js path and sytax errors, as well as moved updateButton to inside of changeState hooks, since when react unmounts it seems to clear all state and variables before executing the cleanup function.

--- frontend/src/Button.js
+++ frontend/src/Button.js
@@ -2,10 +2,14 @@ import {useState,useEffect } from "react"
 
 function Button(props) {
 
-	const [count,changeCount] = useState(props.upvotes);
+	let x = props.upvotes || 0
+	const [count,changeCount] = useState(x);
 
 	useEffect(() => {
 		getButtonData();
+		return () => { 
+			//updateButton(count)
+		}
 	},[]);
 
 	let id = props.id;
@@ -16,19 +20,72 @@ function Button(props) {
 	
 	URL = url + "/button/"+ props.id
 
+	//	console.log("picking postButton")
 	}
+
+	else if (props.replyId) {
+	
+	//	console.log("picking replyButton")
+		URL = url + "/reply/replyButton/" + props.replyId
+
+	}
+
 	else {
 	URL = url + "/replyButton/"+ props.id
 
+	//	console.log("picking replyButton with own id")
 	}
 
+	//console.log(URL)
 	function handleIncrement() {
-		changeCount(c => c = c+1);
+		changeCount(c =>  {
+			c = c+1	
+			updateButton(c);
+			return c;
+		});
 	}
 
 	function handleDecrement() {
-		changeCount(c => c = c-1);
+		changeCount(c => { c = c-1
+		
+			updateButton(c);
+			return c;
+
+		});
 	}
+
+
+
+
+	async function updateButton(upvotes) {
+
+		//console.log(URL);
+		//console.log(id);
+		//console.log(count == undefined);
+		//let upvotes = count;
+		console.log(count);
+		fetch(URL,{
+			method:"PUT",
+			headers: {"Content-type":"application/json"},
+			body: JSON.stringify({
+				id: id,
+				upvotes:upvotes,
+			})
+
+		}).
+			then(response => {
+				if (!response.ok) {	
+					throw new Error("Response status" + response.status + response.message);	
+				console.log(response.status);
+				}
+				
+			}).catch(err => console.log(err));
+	}
+
+
+
+
+
 
 	async function getButtonData() {
 
@@ -42,9 +99,13 @@ function Button(props) {
 			}).
 			then(data => 
 				{
-					changeCount(c => c = data.upvotes);
-
-				}).catch(err => console.log(err));
+					//console.log(data);
+					id = data.button.id;
+					changeCount(c => c = data.button.upvotes);
+					if (props.postId) {URL = url + "/button/"+ id}
+					else if (props.replyId) {
+						URL = url + "/replyButton/"+ id}
+			}).catch(err => console.log(err));
 	}
 
 
@@ -63,4 +124,4 @@ function Button(props) {
 
 
 
-export Default Button;
+export default Button;
