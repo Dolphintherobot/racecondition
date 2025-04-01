0e9c7ccae353c950607b6b9d51ebf49eb1f8df77
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-01 14:43:14 -0600

    Changed Id to be a useState parameter, as when react woudl re-render the component it would re-execute the function, causing id variable to change when it is not supposed to.

--- frontend/src/Button.js
+++ frontend/src/Button.js
@@ -5,25 +5,23 @@ function Button(props) {
 	let x = props.upvotes || 0
 	const [count,changeCount] = useState(x);
 
+	const [id,changeId] = useState(props.id)
 	useEffect(() => {
 		getButtonData();
-		return () => { 
-			//updateButton(count)
-		}
 	},[]);
 
-	let id = props.id;
+	
 	let url = process.env.URL || "http://localhost:3002"
 
+	console.log("My Id is " + id)
 	let URL = ""
 	if (props.postId) {
-	
 	URL = url + "/button/"+ props.id
 
 	//	console.log("picking postButton")
 	}
 
-	else if (props.replyId) {
+	else if (props.replyId && id == undefined) {
 	
 	//	console.log("picking replyButton")
 		URL = url + "/reply/replyButton/" + props.replyId
@@ -58,12 +56,11 @@ function Button(props) {
 
 
 	async function updateButton(upvotes) {
-
-		//console.log(URL);
-		//console.log(id);
+		console.log(URL);
+		console.log("my Id update is " +id);
 		//console.log(count == undefined);
 		//let upvotes = count;
-		console.log(count);
+		//console.log(count);
 		fetch(URL,{
 			method:"PUT",
 			headers: {"Content-type":"application/json"},
@@ -75,8 +72,7 @@ function Button(props) {
 		}).
 			then(response => {
 				if (!response.ok) {	
-					throw new Error("Response status" + response.status + response.message);	
-				console.log(response.status);
+					return response.json().then(text => {throw new Error("Response status" + response.status + text.message + text.error)});	
 				}
 				
 			}).catch(err => console.log(err));
@@ -100,11 +96,17 @@ function Button(props) {
 			then(data => 
 				{
 					//console.log(data);
-					id = data.button.id;
+					//id = data.button.id;
+					changeId(id => id = data.button.id);
 					changeCount(c => c = data.button.upvotes);
 					if (props.postId) {URL = url + "/button/"+ id}
-					else if (props.replyId) {
-						URL = url + "/replyButton/"+ id}
+					else {
+						URL = url + "/replyButton/"+ id
+					
+						//console.log("Changing id to " +id);
+						console.log(URL)
+					}
+
 			}).catch(err => console.log(err));
 	}
 
