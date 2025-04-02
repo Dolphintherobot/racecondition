995cafdf51345a9e4b3b219fab0ad35f672ec16a
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-01 18:59:39 -0600

    Implemented getRank function.

--- frontend/src/Rank.js
+++ frontend/src/Rank.js
@@ -6,9 +6,12 @@ export function Rank(props) {
 	const [count,changeCount] = useState(0);
 	let author = props.author
 
+	let url = process.env.URL || "http://localhost:3002"
+	const URL = url + "/profile/score/"+ author
 
 	useEffect(() =>{
 
+		getRank();
 
 	},[])
 
@@ -17,6 +20,38 @@ export function Rank(props) {
 	//and then will update the rank accordingly
 	function getRank() {
 	
+		fetch(URL).
+			then(response => {
+				if (!response.ok) {	
+				throw new Error("Response status" + response.status + text.message + text.error)	
+				}
+				return response.json();
+				
+			}).
+			then(data => {
+			
+				let upvotes = data.upvotes
+				changeCount(c = > c = upvotes)
+				let temp = ""
+				if (upvotes < 0) {
+					temp = "needs to change major and/or disciplines"
+				}
+				else if (upvotes < 5) {
+					temp = "noob"
+				}
+				else if (upvotes < 10) {
+					temp = "decent"
+				}
+				else {
+					temp = "programming God"
+				}
+				changeRank(r => r = temp);
+
+			})
+			.catch(err => console.log(err));
+	}
+
+
 
 
 
