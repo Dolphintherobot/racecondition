ff7830c38c28fdf41b12e04c853a8378b2b03b9f
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-01 18:52:13 -0600

    Initial commit.

---
+++ frontend/src/Rank.js
@@ -0,0 +1,29 @@
+import {useState,useEffect} from "react"
+
+export function Rank(props) {
+
+	const [rank,changeRank] = useState("");
+	const [count,changeCount] = useState(0);
+	let author = props.author
+
+
+	useEffect(() =>{
+
+
+	},[])
+
+
+	//grabs the number of upvotes an author has 
+	//and then will update the rank accordingly
+	function getRank() {
+	
+
+
+
+	}
+
+
+
+
+
+}
