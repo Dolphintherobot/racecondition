2b530b9d8b5a494d442fe8108e944ce071b61cd3
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-08 15:02:04 -0600

    Added basic file functions.

--- database/file.js
+++ database/file.js
@@ -1,16 +1,45 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
-const filePath = process.env.filePath || "/app/files"
+const filePath = process.env.filePath || "/app/files/"
 
 
+
+
+async function readFile(name) {
+
+	name = filePath + name;
+
+	try{
+		let temp =  await fs.readFile(name);
+		return temp
+		}
+	catch (err) {
+		console.log(err);
+	}
+}
 
 /*
  * Given a buffer and a name
  * will create a file to stash it away in
+ * assumes the filepath is not passed in and will be added in
  */
 function createFile(name,buffer) {
 
-	
+	name = filePath + name
+
+	try {
+	await fs.writeFile(name,buffer, {flag:"w",encoding:"base64"}); 
+	}
+	catch (err) {
+		console.log(err)
+	}
 
 
+}
+
+/*
+ * Given a sql id will turn it into a string with the extension .jpg
+ */
+function idToJpgName(id) {
+	return id.toString() + ".jpg"
 }
