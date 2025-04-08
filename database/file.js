9f2e56cb75881ee7834aba1ff9034e9a4bc41156
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2025-04-08 14:45:12 -0600

    Initial commit.

---
+++ database/file.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const filePath = process.env.filePath || "/app/files"
+
+
+
+/*
+ * Given a buffer and a name
+ * will create a file to stash it away in
+ */
+function createFile(name,buffer) {
+
+	
+
+
+}
